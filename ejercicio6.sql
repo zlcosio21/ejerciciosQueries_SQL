9d5d2e27d578bbcf8890b4477cbf0579ebe9909f
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-07-06 13:38:37 -0500

    ejercicio6.sql

--- ejercicio6.sql
+++ ejercicio6.sql
@@ -1,1 +1,1 @@
-SELECT clientes.nombre, hoteles.nombre_hotel FROM Reservas JOIN clientes ON reservas.cliente_id = clientes.id JOIN hoteles ON reservas.hotel_id = hoteles.id WHERE clientes.nombre = "Zohan Dvir";
+SELECT clientes.nombre, hoteles.nombre FROM Reservas JOIN clientes ON reservas.cliente_id = clientes.id JOIN hoteles ON reservas.hotel_id = hoteles.id WHERE clientes.id = 4;
