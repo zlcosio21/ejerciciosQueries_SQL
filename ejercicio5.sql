1f25d082e8edfdefe0ebe73efa9550e939fb8749
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-07-06 13:33:47 -0500

    Update ejercicio5.sql

--- ejercicio5.sql
+++ ejercicio5.sql
@@ -1,1 +1,1 @@
-SELECT hoteles.nombre_hotel, habitaciones.tipo FROM habitaciones JOIN hoteles ON hoteles.id = habitaciones.hotel_id WHERE tipo = "presidencial"  AND precio > 200.000;
+SELECT nombre, tipo FROM habitaciones JOIN hoteles ON hoteles.id = habitaciones.hotel_id WHERE tipo = "presidencial"  AND precio > 200.000;
