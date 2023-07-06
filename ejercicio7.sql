304c9ac78a7762f5850aa52a70fa9444425a3ff3
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-07-06 13:46:29 -0500

    ejercicio7.sql

--- ejercicio7.sql
+++ ejercicio7.sql
@@ -1,1 +1,1 @@
-SELECT clientes.nombre, hoteles.nombre_hotel FROM reservas JOIN clientes ON reservas.cliente_id = clientes.id JOIN hoteles ON reservas.hotel_id = hoteles.id WHERE clientes.nombre = "Franco Armani" AND hoteles.nombre_hotel = "Hotel Sisa";
+SELECT clientes.nombre, hoteles.nombre, fecha_inicio, fecha_fin FROM reservas JOIN clientes ON reservas.cliente_id = clientes.id JOIN hoteles ON reservas.hotel_id = hoteles.id WHERE clientes.id = 1 AND hoteles.nombre = "Hotel Sisa";
