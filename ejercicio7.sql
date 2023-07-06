9af55b1d3c112e9e3b621dba6081b99ac7e764b6
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-07-06 14:04:57 -0500

    ejercicio7.sql

--- ejercicio7.sql
+++ ejercicio7.sql
@@ -1,1 +1,5 @@
+/*
+Consultar todas las reservas que Franco Armani tiene en el Hotel Sisa. Se debe mostrar nombre de cliente y nombre del hotel en la consulta
+*/
+
 SELECT clientes.nombre, hoteles.nombre, fecha_inicio, fecha_fin FROM reservas JOIN clientes ON reservas.cliente_id = clientes.id JOIN hoteles ON reservas.hotel_id = hoteles.id WHERE clientes.id = 1 AND hoteles.id = 4;
