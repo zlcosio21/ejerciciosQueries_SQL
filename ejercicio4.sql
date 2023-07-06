60b0818e578712e06b256e89601dc6058a4c8ce3
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-07-06 13:59:59 -0500

    ejercicio4.sql

--- ejercicio4.sql
+++ ejercicio4.sql
@@ -1,1 +1,5 @@
+/*
+Consultar todas los habitaciones individuales. Se debe mostrar el nombre del hotel en la consulta.
+*/
+
 SELECT nombre, tipo FROM habitaciones JOIN hoteles ON hoteles.id = habitaciones.hotel_id WHERE tipo = "individual";
