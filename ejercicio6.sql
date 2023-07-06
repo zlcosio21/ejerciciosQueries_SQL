0e77b4e4e2a992cac8099af6333569483146eaf9
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-07-06 14:03:32 -0500

    ejercicio6.sql

--- ejercicio6.sql
+++ ejercicio6.sql
@@ -1,1 +1,5 @@
-SELECT clientes.nombre, hoteles.nombre FROM Reservas JOIN clientes ON reservas.cliente_id = clientes.id JOIN hoteles ON reservas.hotel_id = hoteles.id WHERE clientes.id = 4;
+/*
+Consultar todas las reservas de Zohan Dvir. Se debe mostrar nombre de cliente y nombre del hotel en la consulta.
+*/
+
+SELECT clientes.nombre, hoteles.nombre, fecha_inicio, fecha_fin FROM reservas JOIN clientes ON reservas.cliente_id = clientes.id JOIN hoteles ON reservas.hotel_id = hoteles.id WHERE clientes.id = 4;
