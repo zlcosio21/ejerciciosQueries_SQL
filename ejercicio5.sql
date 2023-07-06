eddd6e21266a309616bed254368383b2d28c2d5f
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-07-06 14:01:33 -0500

    ejercicio5.sql

--- ejercicio5.sql
+++ ejercicio5.sql
@@ -1,1 +1,6 @@
+/*
+Consultar todas las habitaciones presidenciales cuyo precio sea mayor a $200,000.
+Se debe mostrar el nombre del hotel en la consulta..
+*/
+
 SELECT nombre, tipo FROM habitaciones JOIN hoteles ON hoteles.id = habitaciones.hotel_id WHERE tipo = "presidencial"  AND precio > 200.000;
