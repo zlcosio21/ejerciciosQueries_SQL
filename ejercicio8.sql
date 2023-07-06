958938e8c9084069755662b4b0b75282b4cb52ea
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-07-06 14:06:09 -0500

    ejercicio8.sql

--- ejercicio8.sql
+++ ejercicio8.sql
@@ -1,1 +1,5 @@
+/*
+Consultar todas las reservas cuya fecha de inicio sea mayor al 15 de junio de 2023 (2023-06-15). Se debe mostrar nombre de cliente y nombre del hotel en la consulta.
+*/
+
 SELECT clientes.nombre, hoteles.nombre, fecha_inicio, fecha_fin FROM reservas JOIN clientes ON cliente_id = clientes.id JOIN hoteles ON hotel_id = hoteles.id WHERE fecha_inicio > "2023-06-15";
